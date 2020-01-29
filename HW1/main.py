732517770d83d0f41ca9737e1dbee384512cc7f8
AdkPete <pac4607@rit.edu>
2020-01-29 15:39:51 -0500

    Improved plot code

--- HW1/main.py
+++ HW1/main.py
@@ -18,6 +18,11 @@ def lens(x , wlength , N0 , D , a):
 	#print ((C).to(u.dimensionless_unscaled))
 	return x * (1 + C)
 	
+def L2(x , wlength , N0 , D , a , rc):
+	re = 2.817940328e-15 * u.m
+	C = ((wlength ** 2 * N0 * re * D) /(np.pi * a * a)) * ((1 + (x / rc) ** 2) ** (-0.5))
+	return x * (1 + C)
+	
 def problem_3():
 	
 	thresh = 1
@@ -36,11 +41,11 @@ def problem_3():
 		thresh /=1.5
 	print (nv , b_value , s_value)
 	print (f(nv) , f(b_value) , f(s_value))
-	plt.plot(th , bi_iter , label = "bisection")
-	plt.plot(th , s_iter , label = "secant")
+	plt.plot(th , bi_iter , label = "Bisection")
+	plt.plot(th , s_iter , label = "Secant")
 	plt.plot(th , n_iter , label = "Newton")
 	plt.legend()
-	plt.xlabel("log(threshhold)")
+	plt.xlabel("log(threshold)")
 	plt.ylabel("Number of Iterations")
 	plt.savefig("z.pdf")
 	plt.close()
@@ -50,18 +55,34 @@ def problem_4():
 	a = 1 * u.au
 	lamb = 21 * u.cm
 	N_0 = .01 * u.pc / (u.cm ** 3)
+	rc = 1 * u.au
 	
-	x = [-2 * u.au]
+	x = [-10 * u.au]
 	xp = [lens(x[-1] , lamb , N_0 , D , a)]
-	while x[-1].value < 2:
-		x.append(x[-1]+ .01 *  u.au)
+	xp2 = [L2(x[-1] , lamb , N_0 , D , a , rc)]
+	while x[-1].value < 10:
+		x.append(x[-1]+ .08 *  u.au)
+		
 		xp.append(lens(x[-1] , lamb , N_0 , D , a))
+		xp2.append(L2(x[-1] , lamb , N_0 , D , a , rc))
 	
 	for i in range(len(x)):
 		plt.plot([x[i].value , xp[i].value] , [1 , 0] , color = 'b' , linewidth = .5)
-	plt.xlim(0 , 2)
+	#plt.xlim(-5 , 5)
+	plt.xlabel("x (AU)")
+	plt.ylabel("Distance (Kpc)")
 	plt.savefig("4.pdf")
+	
 	plt.close()
+	
+	for i in range(len(x)):
+		plt.plot([x[i].value , xp2[i].value] , [1 , 0] , color = 'b' , linewidth = .5)
+	#plt.xlim(0 , 5)
+	plt.xlabel("x (AU)")
+	plt.ylabel("Distance (Kpc)")
+	plt.savefig("5.pdf")
+	plt.close()
+	
 	print (xp[i])
 	
 		
