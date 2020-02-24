59bb1c854525f26035434e2ba141c9bdc4ba8fc6
AdkPete <pac4607@rit.edu>
2020-02-24 15:12:04 -0500

    Updated various pieces of plotting code

--- HW3/main.py
+++ HW3/main.py
@@ -104,29 +104,27 @@ def WD_func(r , y):
 	return [dp_dr , dm_dr]
 	
 def NS_func(r , y):
-	
-	Pressure = y[0] * u.Ba
-	Mass = y[1] * u.g
-	
-	r *= u.cm
+
+###r is a radius in cm
+
+	Pressure = y[0]
+	Mass = y[1]
 	if Pressure < 0:
 		return [0 , 0]
-	G = (const.G.to(u.cm ** 3 / (u.g * u.s ** 2)))
-	rho = NSrho((Pressure)).to(u.g / (u.cm ** 3))
-	c = const.c.to(u.cm / (u.s))
+	G = (const.G.to(u.cm ** 3 / (u.g * u.s ** 2))).value
+	rho = NSrho((Pressure * u.Ba).to(u.GPa)).to(u.g / (u.cm ** 3)).value
+	c = const.c.to(u.cm / (u.s)).value
 	
-	if Mass.value == 0:
-		dm_dr = 4 * np.pi * r ** 2 * rho
-		return [ 0 , dm_dr.value ]
+	if Mass == 0:
+		dp_dr = 0
 	else:
-		dp_dr = -1 * G * Mass * rho / (r ** 2)
-		dp_dr *= (1 + Pressure / (rho * c ** 2))
-		dp_dr *= (1 + 4 * np.pi * (r ** 3) * Pressure / (Mass * c ** 2))
-		dp_dr *= (1 - 2 * G * Mass / (r * c ** 2)) ** (-1)
-	
+		dp_dr = (-1 * G * Mass * rho / (r ** 2))
+		dp_dr *= (1 + Pressure / (rho * c * c))
+		dp_dr *= (1 + 4 * np.pi * r ** 3 * Pressure / (Mass * c * c))
+		dp_dr *= (1 - 2 * G * Mass / (r * c * c)) ** -1
 	dm_dr = 4 * np.pi * r ** 2 * rho
-	
-	return [ dp_dr.value , dm_dr.value]
+
+	return [dp_dr , dm_dr]
 	
 def problem_2():
 	b = 0.25
@@ -138,9 +136,9 @@ def problem_2():
 	
 	###Now we use my codes
 	A = ode.solve_ode(my_pend(b , c) , 0 , y0 , (t[1] - t[0]))
-	mt , my = A.Heun(t[-1])
-	mt , my = A.Forward_Euler(t[-1])
-	mt , my = A.RK4(t[-1])
+	t_heun , y_heun = A.Heun(t[-1])
+	t_fe , y_fe = A.Forward_Euler(t[-1])
+	t_rk4 , y_rk4 = A.RK4(t[-1])
 	print (sol[0])
 	pt = []
 	ptheta = []
@@ -148,20 +146,43 @@ def problem_2():
 		pt.append(t[i])
 		ptheta.append(sol[i][0])
 		
-	pmy =[]
-	for i in my:
-		pmy.append(i[0])
-	plt.plot(pt , ptheta , label = "scipy")
-	plt.plot(mt , pmy , label = "FE")
-	plt.legend()
+	fe = []
+	for i in y_fe:
+		fe.append(i[0])
+		
+	heun = []
+	for i in y_heun:
+		heun.append(i[0])
+	
+	rk4 = []
+	for i in y_rk4:
+		rk4.append(i[0])
+	f , ax = plt.subplots(3 , 1 , sharex = True)
+	ax[0].plot(pt , ptheta , label = "scipy")
+	ax[0].plot(t_fe , fe , label = "Forward Euler")
+	#ax[0].set_xlabel("time")
+	ax[0].set_ylabel("theta")
+	ax[0].legend()
+	
+	ax[1].plot(pt , ptheta , label = "scipy")
+	ax[1].plot(t_heun , heun , label = "Heun")
+	#ax[1].set_xlabel("time")
+	ax[1].set_ylabel("theta")
+	ax[1].legend()
+	
+	ax[2].plot(pt , ptheta , label = "scipy")
+	ax[2].plot(t_rk4, rk4 , label = "RK4")
+	ax[2].set_xlabel("time")
+	ax[2].set_ylabel("theta")
+	ax[2].legend()
+
 	plt.show()
 	
-
-def problem_3(L):
-
+def stiff(L):
 	
 	#L = 1
-	stiff_solve = ode.solve_ode(stiff_ode(L) , 0 , [0] , 1e-2)
+	h = 1e-2
+	stiff_solve = ode.solve_ode(stiff_ode(L) , 0 , [0] , h)
 	tend = 5
 	t_h , y_h = stiff_solve.Heun(tend)
 	t_fe , y_fe = stiff_solve.Forward_Euler(tend)
@@ -172,23 +193,77 @@ def problem_3(L):
 		rt.append(t_h[i])
 		ry.append(stiff_sltn(t_h[i] , L))
 		
+	heun = []
+	rk4 = []
+	fe = []
 	
-	f , ax = plt.subplots( 2 , 2 , sharex = True)
-	ax[0 , 0].plot(t_h , y_h , label = "numerical")
-	ax[0 , 0].plot(rt , ry , label = "real")
-	ax[0 , 0].legend()
-	ax[0 , 0].set_title("Heun's Method")
+	t = 0
 	
-	ax[1 , 0].plot(t_fe , y_fe , label = "numerical")
-	ax[1 , 0].plot(rt , ry , label = "real")
-	ax[1 , 0].legend()
-	ax[1 , 0].set_title("Forward Euler Method")
+	for i in y_h:
+		heun.append(abs(i[0] - stiff_sltn(t , L)))
+		t += h
 	
-	ax[0 , 1].plot(t_rk4 , y_rk4 , label = "numerical")
-	ax[0 , 1].plot(rt , ry , label = "real")
-	ax[0 , 1].legend()
-	ax[0 , 1].set_title("RK4 Method")
+	t = 0
+	for i in y_fe:
+		fe.append(abs(i[0] - stiff_sltn(t , L)))
+		t += h
+	t = 0
+	for i in y_rk4:
+		rk4.append(abs(i[0] - stiff_sltn(t , L)))
+		t += h
+		
+	return max(fe) , max(heun) , max(rk4)
+	
+	'''
+	f , ax = plt.subplots( 1 , 3 , sharex = True)
+	ax[0].plot(t_h , heun , label = "numerical")
+	#ax[0].plot(rt , ry , label = "real")
+	#ax[0].legend()
+	ax[0].set_title("Heun's Method")
+	
+	ax[1].plot(t_fe , fe , label = "numerical")
+	#ax[1].plot(rt , ry , label = "real")
+	#ax[1].legend()
+	ax[1].set_title("Forward Euler Method")
+	
+	ax[2].plot(t_rk4 , rk4 , label = "numerical")
+	#ax[2].plot(rt , ry , label = "real")
+	#ax[2].legend()
+	ax[2].set_title("RK4 Method")
 	plt.show()
+	'''
+	
+def problem_3():
+
+	L = 1
+	fe = []
+	h = []
+	Ls = []
+	rk = []
+	while L < 300:
+		a , b  ,c = stiff(L)
+		fe.append(a)
+		h.append(b)
+		rk.append(c)
+		Ls.append(L)
+		L += 1
+	
+
+	f , ax = plt.subplots( 3 , 1 , sharex = True)
+	
+	ax[0].plot(Ls , fe)
+	ax[0].set_title("Forward Euler Method")
+	
+	ax[1].plot(Ls , h)
+	ax[1].set_title("Heun's Method")
+	
+	ax[2].plot(Ls , rk)
+	ax[2].set_title("RK4 Method")
+	plt.show()
+	
+		
+		
+	
 	
 def problem_4():
 	
@@ -229,9 +304,10 @@ def problem_4():
 def problem_5():
 
 	###neutron stars
-	h = 1e4
+	h = 5e4
 	
 	rc = 1e14 * u.g / (u.cm ** 3)
+	Pc = NSPressure(rc)
 	Rad = []
 	M_total = []
 	while rc.value < 1e17:
@@ -239,8 +315,8 @@ def problem_5():
 		
 		Neutron_Star = ode.solve_ode(NS_func , 1 , [P_c , 0] , h)
 		
-		rend = 5e6
-		nsr , nsy = Neutron_Star.RK4(rend)
+		rend = 5e9
+		nsr , nsy = Neutron_Star.Forward_Euler(rend)
 
 		for i in range(len(nsr)):
 			if nsy[i][0] < 0:
@@ -249,12 +325,13 @@ def problem_5():
 				Rad.append((R * u.cm).to(u.km).value)
 				M_total.append( (M * u.g).to(u.M_sun).value)
 				break
+		#print (nsy[-1])
 		rc *= 2
 	plt.plot(M_total , Rad)
 	plt.xlabel("Mass (M_sun)")
-	plt.ylabel("Radius (Solar Radii)")
+	plt.ylabel("Radius (km)")
 	plt.show()
 #problem_2()
-#problem_3(150)
+problem_3()
 #problem_4()
-problem_5()
+#problem_5()
