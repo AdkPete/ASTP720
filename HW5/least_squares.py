34bac1077de4daa26cf3e725730a4e9d9fbb7794
AdkPete <pac4607@rit.edu>
2020-04-08 12:19:55 -0400

    Wrote new matrix class that uses numpy functions. Added some plot code, and added some uncertainties in the test functions for HW 5

--- HW5/least_squares.py
+++ HW5/least_squares.py
@@ -1,22 +1,42 @@
 
-import matrix
-
+import matplotlib.pyplot as plt
+from matrix import matrix
 def lin_fit(y , t):
 	###We are going to solve a matrix problem to find m and b, where y = m * t + b
 	n = len(y)
-	NY = []
-	for i in y:
-		NY.append([i])
-	Y = matrix.Matrix(( n , 1 ) , NY)
-	setup = []
+	Y = matrix(y)
+	X = []
+	
 	for i in range(n):
-		setup.append([1 , t[i]])
-	X = matrix.Matrix( (n , 2) , setup)
+		X.append([1 , t[i]])
+	
+	X = matrix(X)
+	
 	Xdagger = X.transpose()
 	XdagX = Xdagger * X
+	
 	theta = XdagX.inverse() * Xdagger
 	theta *= Y
+o
+	return theta.elements[0] , theta.elements[1]
 	
-	return theta.elements[0][0] , theta.elements[1][0]
+def mk_lin_plot(y , t , m , b):
 	
-
+	start_t = min(t)
+	end_t = max(t)
+	h = (end_t - start_t) / 1000.0
+	tf = start_t
+	
+	fit_t = []
+	fit_y = []
+	
+	while tf < end_t:
+		
+		fit_t.append(tf)
+		fit_y.append(m * tf + b)
+		tf += h
+		
+	plt.plot(fit_t , fit_y)
+	plt.scatter(t , y)
+	plt.show()
+			
--- HW5/test.py
+++ HW5/test.py
@@ -1,12 +1,14 @@
 
 import unittest
 from least_squares import *
+import numpy as np
 
+np.random.seed(42)
 class TestLS(unittest.TestCase):
 
 	def test_least_squares(self):
 		def f(t):
-			return 2 * t + 5 + np.random.rand() / 10.0
+			return 2 * t + 5
 		times = []
 		y = []
 		for i in range(100):
@@ -14,7 +16,23 @@ class TestLS(unittest.TestCase):
 			y.append(f(i))
 			
 		b , m = lin_fit(y , times)
-		self.assertEqual(m , 2)
-		self.assertEqual(b , 5)
+		self.assertAlmostEqual(m , 2)
+		self.assertAlmostEqual(b , 5)
+		
+	def test_with_err(self):
+		def f(t):
+			return 2 * t + 5 + np.random.rand() / 100.0
+		times = []
+		y = []
+		for i in range(10):
+			t = float(i) * 10
+			times.append(t)
+			y.append(f(t))
+		
+		b , m = lin_fit(y , times)
+
+		self.assertEqual(round(m , 0) , 2)
+		self.assertEqual(round(b , 0) , 5)
+		
 		
 unittest.main()
