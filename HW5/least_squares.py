4c22e9dd7cc9990428321db4921eaea93d0bb221
AdkPete <pac4607@rit.edu>
2020-04-08 14:33:40 -0400

    Added code to solve problems with 2 parameters and an extra variable. Wrote some extra test code and plot code.

--- HW5/least_squares.py
+++ HW5/least_squares.py
@@ -1,6 +1,8 @@
 
 import matplotlib.pyplot as plt
 from matrix import matrix
+
+
 def lin_fit(y , t):
 	###We are going to solve a matrix problem to find m and b, where y = m * t + b
 	n = len(y)
@@ -17,9 +19,28 @@ def lin_fit(y , t):
 	
 	theta = XdagX.inverse() * Xdagger
 	theta *= Y
-o
+
 	return theta.elements[0] , theta.elements[1]
 	
+def more_general_fit(y , t1 , t2):
+	###We are going to solve a matrix problem to find m and b, where y = m * t + b
+	n = len(y)
+	Y = matrix(y)
+	X = []
+	
+	for i in range(n):
+		X.append([1 , t1[i] , t2[i]])
+	
+	X = matrix(X)
+	
+	Xdagger = X.transpose()
+	XdagX = Xdagger * X
+	
+	theta = XdagX.inverse() * Xdagger
+	theta *= Y
+
+	return theta.elements[0] , theta.elements[1] , theta.elements[2]
+
 def mk_lin_plot(y , t , m , b):
 	
 	start_t = min(t)
@@ -40,3 +61,22 @@ def mk_lin_plot(y , t , m , b):
 	plt.scatter(t , y)
 	plt.show()
 			
+def mgplot(y , t1 , t2 , a , b , c):
+	
+	start_t = min(t1)
+	end_t = max(t1)
+	h = (end_t - start_t) / 1000.0
+	tf = start_t
+	
+	fit_t = []
+	fit_y = []
+	t2 = 1
+	while tf < end_t:
+		
+		fit_t.append(tf)
+		fit_y.append(a + b * tf + c * t2)
+		tf += h
+		
+	plt.plot(fit_t , fit_y)
+	plt.scatter(t1 , y)
+	plt.show()
--- HW5/test.py
+++ HW5/test.py
@@ -34,5 +34,22 @@ class TestLS(unittest.TestCase):
 		self.assertEqual(round(m , 0) , 2)
 		self.assertEqual(round(b , 0) , 5)
 		
+	def test_2_param(self):
+		def f(t1 , t2):
+			return 5 + 2 * t1 + 3 * t2
+		ti1 = []
+		ti2 = []
+		y = []
+		for i in range(10):
+			for k in range(10):
+				ti1.append(i)
+				ti2.append(k)
+				y.append(f(i , k))
+		a , b , g = more_general_fit(y , ti1 , ti2)
+		mgplot(y , ti1 , ti2 , a , b , g)
+		self.assertEqual(round(a , 0) , 5)
+		self.assertEqual(round(b , 0) , 2)
+		self.assertEqual(round(g , 0) , 3)
+
 		
 unittest.main()
