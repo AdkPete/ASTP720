8af90a0aece4a7a95f3bcb93c642d388e937995d
AdkPete <pac4607@rit.edu>
2020-04-24 15:54:02 -0400

    Added code to initialize the algorithm, by generating an initial creature population

--- Project/genetic_algorithms.py
+++ Project/genetic_algorithms.py
@@ -1,5 +1,5 @@
 import numpy as np
-import sys
+import sys , random
 
 def bin_inc(string , ind):
 
@@ -50,13 +50,22 @@ def float_to_bin(N , p):
 			f -= 2 ** k
 	
 	
+	if N > 0:
+		
+		return "0" + str(dna_i) + "." + dna_f
 	
-	return str(dna_i) + "." + dna_f
+	return "1" + str(dna_i) + "." + dna_f
 	
 def bin_to_float(N):
 	
 	if float(N) == 0:
 		return 0
+	if N[0] == "1":
+		sign = -1
+	else:
+		sign = 1
+	
+	N = N[1::]
 	i = str(N).split(".")[0]
 	
 	
@@ -89,11 +98,12 @@ def bin_to_float(N):
 	res = float(str(v) + "." + str(vf))
 	
 
-	return res
+	return sign * res
+	
 	
 class Genetic:
 
-	def __init__(self , f , precision , creatures = None):
+	def __init__(self , f , precision , bounds , popsize , creatures = None):
 		'''
 		f is a function that takes in an array of N parameters and returns some form of goodness of fit / likelihood or similar.
 		precision determines how large the dna strings will be
@@ -104,8 +114,39 @@ class Genetic:
 		
 		self.f = f
 		self.prec = precision
+		self.bounds = bounds
+		self.popsize = popsize
 		
-		self.creatures = creatures
+		if creatures == None:
+			self.creatures = []
+		else:
+			self.creatures = creatures
+			
+		
+	def create_dna(self , X):
+		'''
+		X is a numpy array
+		Returns a dna string
+		'''
+		dna = ""
+		for i in X:
+			dna += float_to_bin(i , self.prec) + "|"
+		
+		return dna[0:-1]
+		
+			
+	def initialize(self):
+		
+		while len(self.creatures) < self.popsize:
+			
+			X = np.array( [] )
+			for i in self.bounds:
+				parameter = random.uniform(i[0] , i[1])
+				X = np.append(X , parameter)
+			
+			dna = self.create_dna(X)
+			self.creatures.append(creature(dna))
+		
 		
 	
 		
@@ -115,9 +156,15 @@ class creature:
 		self.dna = dna
 		self.fitness = None
 		
+	def get_params(self):
+		X = np.array( [] )
+		for i in self.dna.split("|"):
+			X = np.append(X , bin_to_float(i))
+		return X
+		
 		
 	
-N = 101.253	
-bin = float_to_bin(15.74 , [8 , 8 ])
-numb = bin_to_float(bin)
-print (N)
+A = Genetic(1 , [10 , 10] , [ [0 , 5] , [-1 , 7 ] ] , 20 )
+A.initialize()
+for i in A.creatures:
+	print (i.get_params())
