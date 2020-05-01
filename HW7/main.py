7a2c0f8fc32c7ea805333b5267b68cced56b1903
AdkPete <pac4607@rit.edu>
2020-05-01 12:16:43 -0400

    Finalized hw6 plot code

--- HW7/main.py
+++ HW7/main.py
@@ -7,21 +7,27 @@ def transform():
 	T = len(h) * 60 ###Total observation time in seconds
 
 	Hk , freq = FFT.log_data(h , T)
-
+	
 	np.save("Transform" , [ Hk , freq ])
 	
 	mkplot()
 
 def mkplot():
+
 	A = np.load("Transform.npy")
 	
 	freq = A[1]
 	Hk = A[0]
 	
+	for i in range(len(freq)):
+		freq[i] = 10 ** freq[i]
+		Hk[i] = 10 ** Hk[i]
 	plt.plot(freq , Hk)
-	plt.xlabel("log ( Frequency (Hz) )")
-	plt.ylabel("log ( Hk ) ")
-	plt.xlim(-8 , -2)
+	plt.xlabel("Frequency (Hz)")
+	plt.ylabel("Hk")
+	plt.yscale("log")
+	plt.xscale("log")
+	plt.xlim(10 ** (-8) , 10 ** (-2))
 	plt.savefig("Figure1.pdf")
 	
 def measurements():
@@ -44,22 +50,30 @@ def measurements():
 	peakh = max(rk)
 	
 	f = rf[rk.index(peakh)]
-	#print (f.real)
+	
+
 	f = 10 ** f.real
+	
+	print ("The frequency for or GW signal is {} Hz".format(f))
+
 	amplitude = 10 ** peakh 
+	
+	
 	amp = (np.sqrt(amplitude.real ** 2 + amplitude.imag ** 2) / (2 ** 19))
+	
+	print ("The amplitude for or GW signal is {}".format(amp))
 	
 	R = (amp / (2.6e-21)) * (1e-4 / f ) ** 4 * (D)
 	R = R ** (1./5)
 	
-	print (R)
+	print ("The seperation is R = {} Solar Radii".format(R))
+	
 	
 	
 	M = (R ** 1.5 * f / 1e-4) ** 2
-	print (M)
 	
-	print (f , 1e-4 * np.sqrt(M) * R ** (-1.5))
-	print (amp , 2.6e-21 * M * M * (1. / D) / R)
+	print ("The Mass is M = {} Solar Masses".format(M))
+	
 	
 def run_filter():
 	h = np.load("strain.npy")
@@ -77,11 +91,13 @@ def run_filter():
 	plt.close()
 	
 	
-	plt.plot(t , np.array(h)/1e-18)
+	plt.plot(t , np.array(h)/1e-18 , color = "green")
 	plt.xlabel("Time (days)")
 	plt.ylabel("Strain / 1e-18 ")
 	plt.savefig("Figure3.pdf")
 	
-transform()
+#transform()
 measurements()
-run_filter()
+mkplot()
+#run_filter()
+
